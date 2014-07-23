c14fac1d37aa142441afa4de323cbf296a3a395e
Cydonia7 <cydonia572@gmail.com>
2014-07-23 17:17:49 +0200

    Fixes 2 SensioLabs Insight warnings

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cydo_presenter');
+        $treeBuilder->root('cydo_presenter');
 
         return $treeBuilder;
     }
